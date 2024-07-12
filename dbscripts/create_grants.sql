78ade7612abe7c7dfbe7fa1506b81e4c3f7e5703
ilfur <35766086+ilfur@users.noreply.github.com>
2024-07-12 16:27:57 +0200

    Update create_grants.sql

--- dbscripts/create_grants.sql
+++ dbscripts/create_grants.sql
@@ -6,3 +6,4 @@ grant all on sys.saga_participant$ to pdb_adm;
 grant all on sys.saga$ to pdb_adm;
 grant all on sys.saga_participant_set$ to pdb_adm;
 grant create public database link to pdb_adm;
+grant execute on sys.dbms_saga to pdb_adm;
