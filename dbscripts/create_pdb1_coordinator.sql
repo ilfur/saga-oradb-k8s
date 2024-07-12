ad1cae8107953f49996f299c5ee381b1a6b8c1a3
ilfur <35766086+ilfur@users.noreply.github.com>
2024-07-12 16:13:50 +0200

    Update create_pdb1_coordinator.sql

--- dbscripts/create_pdb1_coordinator.sql
+++ dbscripts/create_pdb1_coordinator.sql
@@ -5,8 +5,8 @@ end;
 //demo1: cloud bank transfer
 
 --begin
---  dbms_saga_adm.add_coordinator(coordinator_name => 'CloudBankCoordinator', mailbox_schema => 'pdb_adm', broker_name => 'TEST', dblink_to_coordinator => 'pdb1_link');
---  dbms_saga_adm.add_participant(participant_name => 'CloudBank', coordinator_name => 'CloudBankCoordinator' , dblink_to_broker => 'pdb1_link' , mailbox_schema => 'pdb_adm' , broker_name => 'TEST', dblink_to_participant => 'pdb1_link');
+--  dbms_saga_adm.add_coordinator(coordinator_name => 'CloudBankCoordinator', mailbox_schema => 'pdb_adm', broker_name => 'TEST', dblink_to_coordinator => null);
+--  dbms_saga_adm.add_participant(participant_name => 'CloudBank', coordinator_name => 'CloudBankCoordinator' , dblink_to_broker => null , mailbox_schema => 'pdb_adm' , broker_name => 'TEST', dblink_to_participant => null);
 --end;
 
 //demo2: travel agency bookings
@@ -34,6 +34,6 @@ end dbms_ta_cbk;
 
 
 begin
-    dbms_saga_adm.add_coordinator(coordinator_name => 'TravelCoordinator', mailbox_schema => 'pdb_adm', broker_name => 'TEST', dblink_to_coordinator => 'pdb1_link');
+    dbms_saga_adm.add_coordinator(coordinator_name => 'TravelCoordinator', mailbox_schema => 'pdb_adm', broker_name => 'TEST', dblink_to_coordinator => null);
     dbms_saga_adm.add_participant(participant_name => 'TravelAgencyPLSQL',   coordinator_name => 'TravelCoordinator' ,   dblink_to_broker => null ,   mailbox_schema => 'pdb_adm' ,   broker_name => 'TEST' ,   callback_package => 'dbms_ta_cbk' ,   dblink_to_participant => null);
 end;
