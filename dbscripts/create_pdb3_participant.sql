1f7f4e077f4b7a948ad5df843c74064606fe806a
ilfur <35766086+ilfur@users.noreply.github.com>
2024-07-12 16:25:02 +0200

    Update create_pdb3_participant.sql

--- dbscripts/create_pdb3_participant.sql
+++ dbscripts/create_pdb3_participant.sql
@@ -9,7 +9,7 @@ create or replace package dbms_car_cbk as
 function request(saga_id in RAW, saga_sender IN VARCHAR2, payload IN JSON DEFAULT NULL) return JSON;
 procedure after_rollback(saga_id in RAW, saga_sender IN varchar2, payload IN JSON DEFAULT NULL);
 end dbms_car_cbk;
-
+/
 
 create or replace package body dbms_car_cbk as
 function request(saga_id in RAW, saga_sender IN VARCHAR2, payload IN JSON DEFAULT NULL) return JSON as
@@ -27,15 +27,17 @@ begin
   update cars set available = available - 1 where id = 1;
   return response;
 end;
+/
 
 procedure after_rollback(saga_id in RAW, saga_sender IN varchar2, payload IN JSON DEFAULT NULL)as
 begin
   update cars set available = available + 1 where id = 1;
 end;
 end dbms_car_cbk;
-
+/
 
 --Add participant
 begin
   dbms_saga_adm.add_participant(participant_name=> 'CarPLSQL' ,  dblink_to_broker=> 'SAGAPDB1.PRIVATEK8SNET.K8SNET.ORACLEVCN.COM',mailbox_schema=> 'pdb_adm',broker_name=> 'TEST', callback_package => 'dbms_car_cbk' , dblink_to_participant=> 'SAGAPDB3.PRIVATEK8SNET.K8SNET.ORACLEVCN.COM');
 end;
+/
