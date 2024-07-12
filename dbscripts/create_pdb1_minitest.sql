8e66119c19c5bb489544ba95f7d2aaa90a59fb9e
ilfur <35766086+ilfur@users.noreply.github.com>
2024-07-12 16:23:20 +0200

    Update create_pdb1_minitest.sql

--- dbscripts/create_pdb1_minitest.sql
+++ dbscripts/create_pdb1_minitest.sql
@@ -9,9 +9,11 @@ begin
   insert into mbtest values(json_serialize(payload));
 end;
 end mb_cbk;
-
+/
+  
 begin
   dbms_saga_adm.add_broker(broker_name => 'MBBroker', broker_schema => 'pdb_adm');
-  dbms_saga_adm.add_coordinator(coordinator_name => 'MBCoordinator', mailbox_schema => 'pdb_adm', broker_name => 'MBBroker', dblink_to_coordinator => 'pdb1_link');
-  dbms_saga_adm.add_participant(participant_name => 'MBInitiator',   coordinator_name => 'MBCoordinator' ,   dblink_to_broker => null ,   mailbox_schema => 'pdb_adm' ,   broker_name => 'MBBroker' ,   callback_package => 'mb_cbk' ,   dblink_to_participant => null);
+  dbms_saga_adm.add_coordinator(coordinator_name => 'MBCoordinator', mailbox_schema => 'pdb_adm', broker_name => 'MBBroker', dblink_to_coordinator => 'SAGAPDB1.PRIVATEK8SNET.K8SNET.ORACLEVCN.COM');
+  dbms_saga_adm.add_participant(participant_name => 'MBInitiator',   coordinator_name => 'MBCoordinator' ,   dblink_to_broker => 'SAGAPDB1.PRIVATEK8SNET.K8SNET.ORACLEVCN.COM' ,   mailbox_schema => 'pdb_adm' ,   broker_name => 'MBBroker' ,   callback_package => 'mb_cbk' ,   dblink_to_participant => 'SAGAPDB1.PRIVATEK8SNET.K8SNET.ORACLEVCN.COM');
 end;
+/
