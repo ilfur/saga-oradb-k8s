c6ab951224441ee91effb6f8a484547e57d4b6fd
ilfur <35766086+ilfur@users.noreply.github.com>
2024-07-12 14:45:02 +0200

    Create create_grants.sql

---
+++ dbscripts/create_grants.sql
@@ -0,0 +1,7 @@
+grant saga_adm_role to pdb_adm;
+grant saga_participant_role to pdb_adm;
+grant saga_connect_role to pdb_adm;
+grant all on sys.saga_message_broker$ to pdb_adm;
+grant all on sys.saga_participant$ to pdb_adm;
+grant all on sys.saga$ to pdb_adm;
+grant all on sys.saga_participant_set$ to pdb_adm;
