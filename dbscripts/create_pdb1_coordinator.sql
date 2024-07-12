3a871e0dc1d387b2c84f9124802957e00398d39f
ilfur <35766086+ilfur@users.noreply.github.com>
2024-07-12 14:48:06 +0200

    Create create_pdb1_coordinator.sql

---
+++ dbscripts/create_pdb1_coordinator.sql
@@ -0,0 +1,39 @@
+begin
+  dbms_saga_adm.add_broker(broker_name => 'TEST', broker_schema => 'pdb_adm');
+end;
+
+//demo1: cloud bank transfer
+
+--begin
+--  dbms_saga_adm.add_coordinator(coordinator_name => 'CloudBankCoordinator', mailbox_schema => 'pdb_adm', broker_name => 'TEST', dblink_to_coordinator => 'pdb1_link');
+--  dbms_saga_adm.add_participant(participant_name => 'CloudBank', coordinator_name => 'CloudBankCoordinator' , dblink_to_broker => 'pdb1_link' , mailbox_schema => 'pdb_adm' , broker_name => 'TEST', dblink_to_participant => 'pdb1_link');
+--end;
+
+//demo2: travel agency bookings
+create table travelagencytest(text VARCHAR2(100));
+
+create or replace package dbms_ta_cbk as
+procedure response(saga_id in raw, saga_sender in varchar2, payload in JSON default null);
+end dbms_ta_cbk;
+
+create or replace package body dbms_ta_cbk as
+procedure response(saga_id in RAW, saga_sender IN varchar2, payload IN JSON DEFAULT NULL) as
+booking_result VARCHAR2(10);
+begin
+  insert into travelagencytest values(saga_sender);
+  insert into travelagencytest values(json_serialize(payload));
+  booking_result := json_value(payload, '$.result');
+  IF booking_result = 'success' THEN
+    dbms_saga.commit_saga('TravelAgencyPLSQL', saga_id);
+  ELSE
+    dbms_saga.rollback_saga('TravelAgencyPLSQL', saga_id);
+  END IF;
+end;
+end dbms_ta_cbk;
+
+
+
+begin
+    dbms_saga_adm.add_coordinator(coordinator_name => 'TravelCoordinator', mailbox_schema => 'pdb_adm', broker_name => 'TEST', dblink_to_coordinator => 'pdb1_link');
+    dbms_saga_adm.add_participant(participant_name => 'TravelAgencyPLSQL',   coordinator_name => 'TravelCoordinator' ,   dblink_to_broker => null ,   mailbox_schema => 'pdb_adm' ,   broker_name => 'TEST' ,   callback_package => 'dbms_ta_cbk' ,   dblink_to_participant => null);
+end;
