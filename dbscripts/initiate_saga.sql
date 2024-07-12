a1d6179b272a1e0a1cb87dc09fd1f2060a146061
ilfur <35766086+ilfur@users.noreply.github.com>
2024-07-12 17:47:41 +0200

    Create initiate_saga.sql

---
+++ dbscripts/initiate_saga.sql
@@ -0,0 +1,9 @@
+declare
+  saga_id RAW(16);
+  request JSON;
+begin
+  saga_id := dbms_saga.begin_saga('TravelAgency');
+  request := json('{"flight":"United"}');
+  dbms_saga.send_request(saga_id, 'Airline', request);
+end;
+/
