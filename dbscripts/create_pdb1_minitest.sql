735a1eef1475c5da2670a15801c9420b6d38e35b
ilfur <35766086+ilfur@users.noreply.github.com>
2024-07-12 14:58:40 +0200

    Create create_pdb1_minitest.sql

---
+++ dbscripts/create_pdb1_minitest.sql
@@ -0,0 +1,17 @@
+create table mbtest (text varchar2(2000));
+create or replace package mb_cbk as
+procedure response(saga_id in raw, saga_sender in varchar2, payload in JSON default null);
+end mb_cbk;
+create or replace package body mb_cbk as
+procedure response(saga_id in RAW, saga_sender IN varchar2, payload IN JSON DEFAULT NULL) as
+begin
+  insert into mbtest values(saga_sender);
+  insert into mbtest values(json_serialize(payload));
+end;
+end mb_cbk;
+
+begin
+  dbms_saga_adm.add_broker(broker_name => 'MBBroker', broker_schema => 'pdb_adm');
+  dbms_saga_adm.add_coordinator(coordinator_name => 'MBCoordinator', mailbox_schema => 'pdb_adm', broker_name => 'MBBroker', dblink_to_coordinator => 'pdb1_link');
+  dbms_saga_adm.add_participant(participant_name => 'MBInitiator',   coordinator_name => 'MBCoordinator' ,   dblink_to_broker => null ,   mailbox_schema => 'pdb_adm' ,   broker_name => 'MBBroker' ,   callback_package => 'mb_cbk' ,   dblink_to_participant => null);
+end;
