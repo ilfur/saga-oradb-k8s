1ee2da33bd584e533dbc9ebb79121056ae19df95
ilfur <35766086+ilfur@users.noreply.github.com>
2024-07-22 13:44:59 +0200

    Update check_sagas.sql

--- dbscripts/check_sagas.sql
+++ dbscripts/check_sagas.sql
@@ -2,4 +2,4 @@ select name, broker_topic, queue_type from dba_saga_brokers;
 select name, broker_name, coordinator from dba_saga_participants;
 
 SELECT start_time, initiator, coordinator, status FROM dba_sagas;
-SELECT start_timeinitiator, coordinator, status FROM dba_hist_sagas;
+SELECT start_time, finalization_time, duration, initiator, coordinator, status FROM dba_hist_sagas;
