dfdd3c40cc63f6b818ba16f86d243c20c867a684
ilfur <35766086+ilfur@users.noreply.github.com>
2024-07-22 14:15:39 +0200

    Update initiate_saga.sql

--- dbscripts/initiate_saga.sql
+++ dbscripts/initiate_saga.sql
@@ -2,7 +2,7 @@ declare
   saga_id RAW(16);
   request JSON;
 begin
-  saga_id := dbms_saga.begin_saga('TravelAgencyPLSQL');
+  saga_id := dbms_saga.begin_saga('TravelAgencyPLSQL', timeout=>10);
   request := json('{"flight":"United"}');
   dbms_saga.send_request(saga_id, 'FlightPLSQL', request);
   request := json('{"hotel":"Accor"}');
